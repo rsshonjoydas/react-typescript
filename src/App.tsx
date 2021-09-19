dff678ce70e0c0a4c814c3fa898f5a61aa13510c
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-19 10:48:13 +0600

    Basic Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import Greet from "./components/Greet";
+import Person from "./components/Person";
+import PersonList from "./components/PersonList";
 import "./styles.css";
 
 const App = () => {
+  const personName = {
+    first: "Shonjoy",
+    last: "Das",
+  };
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: "Wayne",
+    },
+    {
+      first: "Clark",
+      last: "Kent",
+    },
+    {
+      first: "Princess",
+      last: "Diana",
+    },
+  ];
+
   return (
     <>
-      <Greet name="RS" />
+      <Greet name="RS" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </>
   );
 };
